8a1c2a767ca5d1c08ae0b427e86813c1055bd98b
_Chinese_Player_ <whx775852@outlook.com>
2025-09-21 10:06:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -322,7 +322,7 @@ function initScrollToTopButton() {
     const scrollToTopButton = document.createElement('button');
     scrollToTopButton.id = 'scrollToTop';
     scrollToTopButton.className = 'btn-primary';
-    scrollToTopButton.innerHTML = '<i class="fa fa-arrow-up"></i>';
+    scrollToTopButton.innerHTML = '<svg viewBox="0 0 24 24" width="24" height="24" fill="white"><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path></svg>';
     scrollToTopButton.style.cssText = `
         position: fixed;
         bottom: 20px;
